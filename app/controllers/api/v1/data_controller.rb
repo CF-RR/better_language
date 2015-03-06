82e322ccc3bbf2f43ccc4772084cd666f8b30964
Rocky Randalls <rrandalls@gmail.com>
2015-03-06 17:17:40 -0500

    Fix issue with JSON formatting

--- app/controllers/api/v1/data_controller.rb
+++ app/controllers/api/v1/data_controller.rb
@@ -2,11 +2,11 @@ class Api::V1::DataController < ApplicationController
   protect_from_forgery with: :null_session
 
   def index
-    articles = News.new("Afghanistan")
-    # if articles == nil
-    #   render json: "Please specify a language and news topic."
-    # else
-      render json: articles[0]
-    # end
+    @articles = News.new(params[:query])
+    if @articles.nil?
+      render json: "Please specify a language and news topic."
+    else
+      render json: @articles
+    end
   end
 end
--- app/models/news.rb
+++ app/models/news.rb
@@ -2,8 +2,7 @@ class News
 
   def initialize(query)
     url =  "http://api.npr.org/query?apiKey=#{ENV['NPR_TOKEN']}&searchTerm=#{query}
-        &numResults=5&output=JSON&fields=title,teaser"
-    HTTParty.get(url)
+        &numResults=5&fields=title,teaser"
+    @content = HTTParty.get(url)
   end
-
 end
